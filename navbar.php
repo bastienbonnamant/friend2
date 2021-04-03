bb3a8ecfc99bc02112902c857e8792d9b66ef1cb
Bastien Bonnamant <79262493+bastienbonnamant@users.noreply.github.com>
2021-04-03 17:22:57 +0200

    Create navbar.php

---
+++ navbar.php
@@ -0,0 +1,13 @@
+<link href="/navbar.css" rel="stylesheet">
+
+<section class="nav">
+<img src="centralperk.png" alt="centralperk" class="centralperk">
+
+<ul>
+    <li><a href="/index.php">Accueil</a></li>
+    <li><a href="/friend.php">Friend</a></li>
+    <li><a href="/createfriend.php">Ajoute un ami</a></li>
+
+
+</ul>
+</section>
