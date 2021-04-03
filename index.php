4420645bdcea8db61d389b11269f21f15a03c54f
Bastien Bonnamant <79262493+bastienbonnamant@users.noreply.github.com>
2021-04-03 17:22:26 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<link href="/index.css" rel="stylesheet">
+
+
+<?php
+include'./connec.php';
+include'./navbar.php';
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Friends</title>
+</head>
+<body>
+    <section class="body">
+ <h1>SALUUUT</h1>
+ <h2>Clique sur le bouton ci dessous pour ajouter un ami</h2>
+ <a href="createfriend.php" class="bouton">CLIQUE ICI</a>
+ </section>
+
+</body>
+</html>
